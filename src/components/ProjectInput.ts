a39ad8433a1627007919a3d3bd4c9510fbab5cb5
Goktug YILDIZ <goktug.7tepe@gmail.com>
2023-10-14 11:53:06 -0700

    log values in submitted form and add Autobind Decorator

--- src/components/ProjectInput.ts
+++ src/components/ProjectInput.ts
@@ -1,31 +1,76 @@
+const Autobind = (
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
+) => {
+  // console.log("descriptor", descriptor);
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjustedDescriptor;
+};
+
 class ProjectInput {
-    templateElement: HTMLTemplateElement;
-    hostElement: HTMLDivElement;
-    private element: HTMLFormElement;
-    private attach() {
-      this.hostElement.append(this.element);
-    }
-  
-    constructor() {
-      this.templateElement = document.getElementById(
-        "project-input"
-      ) as HTMLTemplateElement;
-      this.hostElement = document.getElementById("app") as HTMLDivElement;
-  
-      console.log(this.templateElement);
-      console.log(this.hostElement);
-      // Get the content of the template
-      const importedNode = document.importNode(
-        this.templateElement.content,
-        true
-      );
-      // Get the form element from the template
-      this.element = importedNode.firstElementChild as HTMLFormElement;
-      // Add a new id to the form
-      this.element.id = "user-input";
-      // Attach the form to the host element
-      this.attach();
-    }
+  //step 1
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  private element: HTMLFormElement;
+  //step 2
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    ) as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app") as HTMLDivElement;
+
+    // Get the content of the template
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    // Get the form element from the template
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    // Add a new id to the form
+    this.element.id = "user-input";
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+    // Attach the form to the host element
+    this.attach();
+    this.configure();
   }
-  export default ProjectInput;
-  
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+  private attach() {
+    this.hostElement.append(this.element);
+  }
+
+  @Autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(
+      this.titleInputElement.value,
+      this.descriptionInputElement.value,
+      this.peopleInputElement.value
+    );
+  }
+}
+
+// const prjInput = new ProjectInput();
+export default ProjectInput;
